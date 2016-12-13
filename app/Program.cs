168244dc4c7a393b98e5fffa7b40602f3aa6ead2
Jakub Míšek <jmisek@gmail.com>
2016-12-13 15:43:11 +0100

    sample app
    part of wordpress

---
+++ app/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peachserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory() + "/../wwwroot")
+                .UseUrls("http://*:5004/")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
