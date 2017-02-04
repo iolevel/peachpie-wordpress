24ff6fa169bfaf7b0d60a98db010c2003bab0b2d
Jakub Míšek <jmisek@gmail.com>
2017-02-04 21:30:41 +0100

    more wp files

--- website/wp-admin/includes/noop.php
+++ website/wp-admin/includes/noop.php
@@ -1,89 +1,89 @@
 <?php
 
-/**
- * @ignore
- */
-function __() {}
+// /**
+ // * @ignore
+ // */
+// function __() {}
 
-/**
- * @ignore
- */
-function _x() {}
+// /**
+ // * @ignore
+ // */
+// function _x() {}
 
-/**
- * @ignore
- */
-function add_filter() {}
+// /**
+ // * @ignore
+ // */
+// function add_filter() {}
 
-/**
- * @ignore
- */
-function esc_attr() {}
+// /**
+ // * @ignore
+ // */
+// function esc_attr() {}
 
-/**
- * @ignore
- */
-function apply_filters() {}
+// /**
+ // * @ignore
+ // */
+// function apply_filters() {}
 
-/**
- * @ignore
- */
-function get_option() {}
+// /**
+ // * @ignore
+ // */
+// function get_option() {}
 
-/**
- * @ignore
- */
-function is_lighttpd_before_150() {}
+// /**
+ // * @ignore
+ // */
+// function is_lighttpd_before_150() {}
 
-/**
- * @ignore
- */
-function add_action() {}
+// /**
+ // * @ignore
+ // */
+// function add_action() {}
 
-/**
- * @ignore
- */
-function did_action() {}
+// /**
+ // * @ignore
+ // */
+// function did_action() {}
 
-/**
- * @ignore
- */
-function do_action_ref_array() {}
+// /**
+ // * @ignore
+ // */
+// function do_action_ref_array() {}
 
-/**
- * @ignore
- */
-function get_bloginfo() {}
+// /**
+ // * @ignore
+ // */
+// function get_bloginfo() {}
 
-/**
- * @ignore
- */
-function is_admin() {return true;}
+// /**
+ // * @ignore
+ // */
+// function is_admin() {return true;}
 
-/**
- * @ignore
- */
-function site_url() {}
+// /**
+ // * @ignore
+ // */
+// function site_url() {}
 
-/**
- * @ignore
- */
-function admin_url() {}
+// /**
+ // * @ignore
+ // */
+// function admin_url() {}
 
-/**
- * @ignore
- */
-function home_url() {}
+// /**
+ // * @ignore
+ // */
+// function home_url() {}
 
-/**
- * @ignore
- */
-function includes_url() {}
+// /**
+ // * @ignore
+ // */
+// function includes_url() {}
 
-/**
- * @ignore
- */
-function wp_guess_url() {}
+// /**
+ // * @ignore
+ // */
+// function wp_guess_url() {}
 
 if ( ! function_exists( 'json_encode' ) ) :
 /**
