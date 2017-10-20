6aa03fff5de7747b4aaa91fe5133a30889a96db3
Jakub Míšek <jmisek@gmail.com>
2017-10-20 13:51:52 +0200

    sample wp plugin in C#

---
+++ dotnet-wp-plugin/plugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pchp.Core;
+
+public static class MyWpPlugin
+{
+    public static void Register(Context ctx)
+    {
+        ctx.Call("add_filter", "the_content", new Func<string, string>(text =>
+        {
+            text = text
+                .Replace("WordPress", "WordPress running on .NET")
+                .Replace("Welcome", "Greetings");
+
+            return text;
+        }));
+    }
+}
---
+++ website/wp-content/plugins/peachpieplugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Peachpie Plugin
+ * @version 1.0
+ */
+/*
+Plugin Name: dotNET WordPress Plugin
+Plugin URI: http://www.peachpie.io/
+Description: Plugin for WordPress in C#
+Author: Jakub Misek
+Version: 1.0
+*/
+
+\MyWpPlugin::Register();
